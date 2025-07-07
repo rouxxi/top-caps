d3e1425372516f66f7feabdd923212f2390879f4
rouxxi <laskar.gwenvael@gmail.com>
2025-07-07 20:27:46 +0200

    add pawns repository

---
+++ api/repository/pawns.ts
@@ -0,0 +1,75 @@
+import {DatabaseService} from '../services/database.ts';
+
+interface Pawn {
+    id?: number;
+    position_x?: number;
+    position_y?: number;
+    skin?: string;
+    player_id?: number;
+    game_id?: number;
+    created_at?: Date | null;
+}
+
+async function getById (id: number) {
+    const db = await DatabaseService.connect();
+    let { data, error } = await db
+        .from('pawns')
+        .select('*')
+        .eq('id', id)
+
+    if( error) {
+        console.log('error ',error)
+        return null;
+    }
+
+    return data;
+}
+
+async function update (fields: Pawn ) {
+    const {id, ...others} = fields
+    const db = await DatabaseService.connect();
+    const { data, error } = await db
+        .from('pawns')
+        .update({ ...others })
+        .eq('id', id)
+        .select('*');
+
+    if( error) {
+        console.log('error ',error)
+        return null;
+    }
+
+    return data;
+}
+
+async function create (fields: Pawn ) {
+    const db = await DatabaseService.connect();
+    const { data, error } = await db
+        .from('pawns')
+        .insert({...fields})
+        .select()
+
+    if( error) {
+        console.log('error ',error)
+        return null;
+    }
+
+    return data;
+}
+
+async function deleteFromId (id: number ) {
+    const db = await DatabaseService.connect();
+    const { data, error } = await db
+        .from('pawns')
+        .delete()
+        .eq('id', id);
+
+    if( error) {
+        console.log('error ',error)
+        return null;
+    }
+
+    return data;
+}
+
+export default { getById, update, create, deleteFromId }
