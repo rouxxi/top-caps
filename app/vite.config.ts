aa244da8332a4fadbf5bc205aee3d21aa9543bab
rouxxi <laskar.gwenvael@gmail.com>
2025-08-03 23:49:59 +0200

    feat(app): add allowed hosts and

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  envDir: 'environments/',
   preview: {
-    port: parseInt(JSON.stringify(process.env.VITE_PORT)) || 8080,
-    host: true
+    port: 8080,
+    host: true,
+    allowedHosts: true,
   }
 })
